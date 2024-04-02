653cf8dbf091d48180656dedc1286e377fbe2457
Saqib <mohdsaqib148183@gmail.com>
2024-04-03 02:31:02 +0530

    fix

--- actions/admin.ts
+++ actions/admin.ts
@@ -3,9 +3,9 @@ import { currentUser } from "@/lib/auth";
 import { UserRole } from "@prisma/client";
 export const admin = async () => {
 	const role = await currentUser();
-	if (role !== UserRole.ADMIN) {
-		return { error: "Forbidden!" };
+	if (role === UserRole.ADMIN) {
+		return { success: "Allowed!" };
 	}
 
-	return { success: "Allowed" };
+	return { error: "Forbidden!" };
 };
